a77018ffc4c4e84ef2ce0bfc1e4c20c254a6a249
LiangGui09 <1069098642>
2019-07-19 11:09:56 +0800

    springboot集成eureka模板
    springboot集成eureka模板

---
+++ client-server/src/main/java/com/eury/clientserver/controller/test.java
@@ -0,0 +1,18 @@
+package com.eury.clientserver.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @name : test
+ * @Description:
+ * @author: eury
+ * @date :  2019/7/19
+ */
+@RestController
+public class test {
+
+
+    public Object test(){
+        return "hellow word";
+    }
+}
